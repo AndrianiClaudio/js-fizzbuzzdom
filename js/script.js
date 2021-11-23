d418c5756b51860895f64edae75de365dafa9259
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-23 15:41:24 +0100

    stampa numeri in appositi div > span

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// stampa numeri da 1 a 100
+const container = document.querySelector('.container'); //acquisisco il mio container
+//ciclo: stampa numeri o stringa a seconda di condizioni
 for(let i = 1; i <= 100; i++) {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    // inserisci span come figlio di div
+    div.append (span);
+    // inserisce come ultimo figlio il nuovo div al container
+    container.append(div);
+    // -- ISTRUZIONE CONDIZIONALE PER LA STAMPA --
     if (i % 3 == 0 && i % 5 == 0) { //numero divisibile sia per 3 che per 5. stampa FizzBuzz
         console.log('FizzBuzz');
+        span.innerHTML = 'FizzBuzz';
     } else if (i % 3 == 0) { //numero divisibile per 3.stampa Fizz
         console.log('Fizz');
+        span.innerHTML = 'Fizz';
     } else if (i % 5 == 0) { //numero divisibile per 5. stampa Buzz
         console.log('Buzz');
+        span.innerHTML = 'Buzz';
     } else { //numero non divisibile né per 3 né per 5. stampa il numero
         console.log(i);
+        span.innerHTML = i;
     }
 }
