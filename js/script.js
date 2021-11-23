0da815e4e8df4139c8403f5944acbaa1ba93be23
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-23 16:24:33 +0100

    Completamento esercizio

--- js/script.js
+++ js/script.js
@@ -10,15 +10,16 @@ for(let i = 1; i <= 100; i++) {
     // inserisce come ultimo figlio del container il nuovo div
     container.append(div);
     if (i % 3 == 0 && i % 5 == 0) { //numero divisibile sia per 3 che per 5
-        div.classList.add('fizz-color')
+        div.classList.add('fizzbuzz-color')
         span.innerHTML = 'FizzBuzz';
     } else if (i % 3 == 0) { //numero divisibile per 3
-        div.classList.add('buzz-color')
+        div.classList.add('fizz-color')
         span.innerHTML = 'Fizz';
     } else if (i % 5 == 0) { //numero divisibile per 5
-        div.classList.add('fizzbuzz-color')
+        div.classList.add('buzz-color')
         span.innerHTML = 'Buzz';
     } else { //numero non divisibile né per 3 né per 5
+        div.classList.add('base-color')
         span.innerHTML = i;
     }
 }
